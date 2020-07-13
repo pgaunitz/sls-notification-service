a23894664867460874fddbe832f7fc0190ef23db
Philip Gaunitz <fluffen@pgaunitz.se>
2020-07-13 17:09:06 +0200

    successfully sent a hard coded email using SES

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,6 +1,32 @@
+import AWS from "aws-sdk";
+
+const ses = new AWS.SES({ region: "eu-west-1" });
+
 async function sendMail(event, context) {
-  console.log(event);
-  return event;
+  const params = {
+    Source: "fluffen@pgaunitz.se",
+    Destination: {
+      ToAddresses: ["fluffen@pgaunitz.se"],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: "Hello from my send mail function!",
+        },
+      },
+      Subject: {
+        Data: "Test Mail",
+      },
+    },
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export const handler = sendMail;
